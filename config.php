bf3dc4750d4a1c12a971ec1b0a712d48c004677b
Albert des de info1 <albert.santiago@ins-mediterrania.cat>
2026-01-15 17:25:57 +0100

    canvis en permisos, crec que res mes

--- config.php
+++ config.php
@@ -20,7 +20,7 @@ session_start();
 $db_host = 'localhost';
 $db_name = 'album_captures';
 $db_user = 'album_user';
-$db_pass = 'YOUR_DB_PW';
+$db_pass = 'USER_PW';
 
 $mysqli = new mysqli($db_host, $db_user, $db_pass, $db_name);
 
